076a1a777b3604c37322176acb7cb390eeaab45d
ignatiuschik <ignatiuschik@gmail.com>
2024-10-14 23:22:53 +0200

    I have added two components, ie Square and Input

---
+++ src/Input.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Input = () => {
+  return (
+    <form action="">
+        <label htmlFor=""></label>
+        <input 
+            type="text"
+            required
+            placeholder='Enter Color of Choice'    
+        />
+    </form>
+  )
+}
+
+export default Input
